fc32cc504546cb3f770ee10acbcc4d6cb13ca936
litchi <yikelitchi@gmail.com>
2021-07-12 15:20:41 +0800

    update build gradle

--- app/src/main/java/com/endura/pos_system/databse/data_MySQL1.java
+++ app/src/main/java/com/endura/pos_system/databse/data_MySQL1.java
@@ -1,17 +1,6 @@
 package com.endura.pos_system.databse;
 
-import android.util.Log;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class data_MySQL1 extends AppCompatActivity {
 
